28922ebb30d3445a5def85104b0512f14e8e9083
Harsh Mahajan <harshmahajan2345@gmail.com>
2025-01-22 17:44:09 +0530

    corrected styles

--- frontend/src/app/(components)/Sidebar/index.tsx
+++ frontend/src/app/(components)/Sidebar/index.tsx
@@ -6,9 +6,18 @@ const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
  const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
-    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white`
+    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64`
   return (
-    <div>Sidebar</div>
+    <div className={sidebarClassNames}>
+        <div className='flex h-[100%] w-full flex-col justify-start'>
+            {/* Top logo */}
+            <div className='z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 '>
+                <div className='text-xl font-bold text-gray-800 dark:text-white'>
+                    SPRINTBOARD
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
